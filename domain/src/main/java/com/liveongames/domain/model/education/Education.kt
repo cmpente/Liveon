336ba9d6805ffddd8d70df458ec5756e4bf2c04b
cmpentecost92 <cmpentecost92@gmail.com>
2025-08-14 07:42:00 +0000

    Alpha.Build.08140241

---
+++ domain/src/main/java/com/liveongames/domain/model/education/Education.kt
@@ -0,0 +1,8 @@
+package com.liveongames.domain.model.education
+
+data class Education(
+    val id: String,
+    val name: String,
+    val year: Int,
+    // Add other relevant properties here, e.g., degree, major, institution, GPA, etc.
+)
